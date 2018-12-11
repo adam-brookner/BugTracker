ae400cb369a3dca112e5f81214a66306a813f388
adam-brookner <45432541+adam-brookner@users.noreply.github.com>
2018-12-11 17:28:15 +0000

    Creating IBugRepository interface and BugRepository class

---
+++ BugTracker/BugTracker/Models/BugRepository.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class BugRepository : IBugRepository
+    {
+        BugEntities bugDB = new BugEntities();
+
+        //Add
+        void IBugRepository.AddBug(BugLog bug)
+        {
+            bugDB.BugLogs.Add(bug);
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.AddDepartment(Department department)
+        {
+            bugDB.Departments.Add(department);
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.AddPrincipal(Principal principal)
+        {
+            bugDB.Principals.Add(principal);
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.AddPriority(Priority priority)
+        {
+            bugDB.Priorities.Add(priority);
+            bugDB.SaveChanges();
+        }
+
+        //Delete
+        void IBugRepository.DeleteBug(BugLog bug)
+        {
+            bugDB.BugLogs.Remove(bug);
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.DeleteDepartment(Department department)
+        {
+            bugDB.Departments.Remove(department);
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.DeletePrincipal(Principal principal)
+        {
+            bugDB.Principals.Remove(principal);
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.DeletePriority(Priority priority)
+        {
+            bugDB.Priorities.Remove(priority);
+            bugDB.SaveChanges();
+        }
+
+        //Get all
+        IEnumerable<BugLog> IBugRepository.GetAllBugs()
+        {
+            return bugDB.BugLogs.ToList();
+        }
+
+        IEnumerable<Department> IBugRepository.GetAllDepartments()
+        {
+            return bugDB.Departments.ToList();
+        }
+
+        IEnumerable<Principal> IBugRepository.GetAllPrincipals()
+        {
+            return bugDB.Principals.ToList();
+        }
+
+        IEnumerable<Priority> IBugRepository.GetAllPriorities()
+        {
+            return bugDB.Priorities.ToList();
+        }
+
+        //Get By ID
+        BugLog IBugRepository.GetBugByID(int BugID)
+        {
+            return bugDB.BugLogs.Single(b => b.BugID == BugID);
+        }
+
+        Department IBugRepository.GetDepartmentByID(int DepartmentID)
+        {
+            return bugDB.Departments.Single(d => d.DepartmentID == DepartmentID);
+        }
+
+        Principal IBugRepository.GetPrincipalByID(int UserID)
+        {
+            return bugDB.Principals.Single(p => p.UserID == UserID);
+        }
+
+        Priority IBugRepository.GetPriorityByID(int PriorityID)
+        {
+            return bugDB.Priorities.Single(pr => pr.PriorityID == PriorityID);
+        }
+
+        //Update / Edit
+        void IBugRepository.UpdateBug(BugLog bug)
+        {
+            BugLog tmpBug = bugDB.BugLogs.Single(b => b.BugID == bug.BugID);
+
+            tmpBug.Title = bug.Title;
+            tmpBug.Description = bug.Description;
+            tmpBug.Created = bug.Created;
+            tmpBug.UserID = bug.UserID;
+            tmpBug.PriorityID = bug.PriorityID;
+
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.UpdateDepartment(Department department)
+        {
+            Department tmpDepartment = bugDB.Departments.Single(d => d.DepartmentID == department.DepartmentID);
+            tmpDepartment.DepartmentName = department.DepartmentName;
+            tmpDepartment.DepartmentManager = department.DepartmentManager;
+
+            bugDB.SaveChanges();
+        }
+
+        void IBugRepository.UpdatePrincipal(Principal principal)
+        {
+            Principal tmpPrincipal = bugDB.Principals.Single(p => p.UserID == principal.UserID);
+            tmpPrincipal.Forename = principal.Forename;
+            tmpPrincipal.Surname = principal.Surname;
+            tmpPrincipal.Address = principal.Address;
+            tmpPrincipal.PostCode = principal.PostCode;
+            tmpPrincipal.Phone_Number = principal.Phone_Number;
+            tmpPrincipal.JobDescription = principal.JobDescription;
+            tmpPrincipal.DepartmentID = principal.DepartmentID;
+
+            bugDB.SaveChanges();
+
+        }
+
+        void IBugRepository.UpdatePriority(Priority priority)
+        {
+            Priority tmpPriority = bugDB.Priorities.Single(pr => pr.PriorityID == priority.PriorityID);
+            tmpPriority.PriorityName = priority.PriorityName;
+            tmpPriority.Description = priority.Description;
+
+        }
+    }
+}
---
+++ BugTracker/BugTracker/Models/IBugRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public interface IBugRepository 
+    {
+        //Get all at once
+        IEnumerable<BugLog> GetAllBugs();
+        IEnumerable<Department> GetAllDepartments();
+        IEnumerable<Principal> GetAllPrincipals();
+        IEnumerable<Priority> GetAllPriorities();
+
+        //Get by ID
+        BugLog GetBugByID(int BugID);
+        Department GetDepartmentByID(int DepartmentID);
+        Principal GetPrincipalByID(int UserID);
+        Priority GetPriorityByID(int PriorityID);
+
+        //Create
+        void AddBug(BugLog bug);
+        void AddDepartment(Department department);
+        void AddPrincipal(Principal principal);
+        void AddPriority(Priority priority);
+
+        //Update
+        void UpdateBug(BugLog bug);
+        void UpdateDepartment(Department department);
+        void UpdatePrincipal(Principal principal);
+        void UpdatePriority(Priority priority);
+
+        //Delete
+        void DeleteBug(BugLog bug);
+        void DeleteDepartment(Department department);
+        void DeletePrincipal(Principal principal);
+        void DeletePriority(Priority priority);
+    }
+}
